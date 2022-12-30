b7115c09cde88eaf7d674cbe66fa2c470a1c2257
RajeshwariPatil1100 <rajeshwaripatil1100@gmail.com>
2022-12-30 18:55:10 +0530

    [Rajeshwari] Mood analyser problems

---
+++ MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    internal class MoodAnalyser
+    {
+        public string message;
+
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            if (this.message.Contains("SAD"))
+            {
+                Console.WriteLine("SAD");
+                return "SAD";
+
+            }
+            else
+            {
+                Console.WriteLine("happy");
+                return "HAPPY";
+
+            }
+        }
+    }
+}
--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MoodAnalyser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace MoodAnalyser
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer");
+            MoodAnalyser mo = new MoodAnalyser("i am SAD");
+            mo.AnalyseMood();
+
+
+        }
+    }
+}
